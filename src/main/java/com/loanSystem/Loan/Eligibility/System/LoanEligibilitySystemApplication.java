38a9d26262c5b58c040da806e3469eedb3708399
Durksie <durksie1@gmail.com>
2026-02-27 09:25:12 +0200

    Spring Structure

---
+++ src/main/java/com/loanSystem/Loan/Eligibility/System/LoanEligibilitySystemApplication.java
@@ -0,0 +1,13 @@
+package com.loanSystem.Loan.Eligibility.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanEligibilitySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanEligibilitySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loanSystem/Loan/Eligibility/System/LoanEligibilitySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loanSystem.Loan.Eligibility.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEligibilitySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
